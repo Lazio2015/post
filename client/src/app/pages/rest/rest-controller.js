472200fc887cb936f3f638f594e62356f55fefbd
Lenur Memetov <Lenur.m@wecandevelopit.com>
2016-01-26 17:31:20 +0300

    fixed params

--- client/src/app/pages/rest/rest-controller.js
+++ client/src/app/pages/rest/rest-controller.js
@@ -2,7 +2,7 @@
  * Created by Lenur on 03.01.2016.
  */
 
-function RestCtrl ($scope, $rootScope, RestService) {
+function RestCtrl ($scope, $rootScope, RestService, $sessionStorage) {
     var rest = this;
     rest.types = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'COPY', 'PATCH'];
 
@@ -16,56 +16,55 @@ function RestCtrl ($scope, $rootScope, RestService) {
   //      { type: "PATCH", number: 7 }
   //  ];
 
-    rest.tabs = [{
-        id: 1,
-        name: 'Default tab',
-        active: true,
-        data: {
-            type: rest.types[0],
-            url:''
-            },
-        content: '',
-        params: [
-            {
+    //ParamsList
+        rest.openParams = function() {
+            rest.tabs[$scope.selectedTab].isCollapsed = !rest.tabs[$scope.selectedTab].isCollapsed;
+        };
+
+        rest.addNewParam = function () {
+            rest.tabs[$scope.selectedTab].params.push({
                 key: '',
                 value: ''
-            }
-        ],
-        isCollapsed: false
-    }];
+            });
+        };
 
-    rest.addNewParam = function () {
-        rest.tabs[$scope.selectedTab].params.push({
-            key: '',
-            value: ''
-        });
+        rest.deleteParam = function (index) {
+            rest.tabs[$scope.selectedTab].params.splice(index, 1);
+        };
+
+    rest.getSessionTabs = function() {
+        return $sessionStorage.tabs ? $sessionStorage.tabs : null;
     };
 
-    rest.showParam = function () {
-        rest.tabs[$scope.selectedTab].isCollapsed = true;
-    };
-
-    rest.counter = 1;
     $scope.selectedTab = 0; //set selected tab
-
     //add tab
     rest.addTab = function () {
         rest.counter++;
-        rest.tabs.push({
+        $sessionStorage.tabs.push({
             id: rest.counter,
-            name: 'New tab ' + rest.counter,
-            data : {type: rest.types[0], url: '', content:''}
+            name: 'New tab ',
+            data : {type: rest.types[0], url: '', content:''},
+            content: '',
+            params: [
+                {
+                    key: '',
+                    value: ''
+                }
+            ],
+            isCollapsed: false
         });
-//        console.log(rest.tabs.length);
-        $scope.selectedTab = rest.tabs.length - 1;
-
-
+        $scope.selectedTab = $sessionStorage.tabs.length - 1;
+        rest.tabs = $sessionStorage.tabs;
     };
 
     //delete tab
     rest.deleteTab = function (index) {
-        rest.tabs.splice(index, 1); //remove the object from the array based on index
+        console.log($sessionStorage.tabs);
+        $sessionStorage.tabs.splice(index, 1); //remove the object from the array based on index
         $scope.selectedTab = index - 1;
+        if (!$sessionStorage.tabs[index-1]) {
+            $scope.selectedTab = $sessionStorage.tabs.length-1;
+        }
     };
 
     // to set selectedTab
@@ -74,31 +73,62 @@ function RestCtrl ($scope, $rootScope, RestService) {
     };
 
     rest.sendData = function () {
-        //console.log(rest.allParam);
-        //console.log(rest.tabs[$scope.selectedTab].data);
         //while send 1 obj, need make changes on server side
         // {base: rest.tabs[$scope.selectedTab].data, param: rest.allParam}
         RestService
-            .send(rest.tabs[$scope.selectedTab].data)
+            .send($sessionStorage.tabs[$scope.selectedTab].data)
             .success(function(response) {
-                rest.tabs[$scope.selectedTab].content = response.content;
+                console.log(response);
+                console.log($sessionStorage.tabs[$scope.selectedTab].data);
+                $sessionStorage.tabs[$scope.selectedTab].content = response.content;
 
                 $rootScope.$emit('history: addData', response.history);
             }).error(function(error) {
                 console.log(error);
             });
-
     };
 
     rest.bindEvents = function () {
         $rootScope.$on('history: invoke', function (e, query) {
-            rest.tabs[$scope.selectedTab].data.type = query.type;
-            rest.tabs[$scope.selectedTab].data.url = query.url;
-            rest.tabs[$scope.selectedTab].content = '';
+            $sessionStorage.tabs[$scope.selectedTab].data.type = query.type;
+            $sessionStorage.tabs[$scope.selectedTab].data.url = query.url;
+            $sessionStorage.tabs[$scope.selectedTab].content = '';
         });
     };
 
+    rest.load = function() {
+        if (rest.getSessionTabs() !== null) {
+            rest.tabs = rest.getSessionTabs();
+            var changeCount = 1;
+            angular.forEach(rest.tabs, function(tab){
+                tab.id = changeCount++;
+            });
+            rest.counter = rest.tabs.length;
+        } else {
+            rest.tabs = [{
+                id: 1,
+                name: 'New tab',
+            //    active: true,
+                data: {
+                    type: rest.types[0],
+                    url:''
+                },
+                content: '',
+                params: [
+                    {
+                        key: '',
+                        value: ''
+                    }
+                ],
+                isCollapsed: false
+            }];
+            $sessionStorage.tabs = rest.tabs;
+            rest.counter = 1;
+        }
+    };
+
     rest.resolve = function() {
+        rest.load();
         rest.bindEvents();
     };
 
